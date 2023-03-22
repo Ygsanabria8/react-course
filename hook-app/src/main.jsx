5a35db561a8f850430968f5b1468fe7550239f9e
Yesid Giovanny Sanabria Orjuela <sana803723@siigo.com>
2023-03-21 22:27:30 -0500

    Final sección 11

--- hook-app/src/main.jsx
+++ hook-app/src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from "react-router-dom";
 
 import { HooksApp } from './HooksApp';
 import { CounterApp } from "./01-useState/CounterApp"
@@ -13,12 +14,15 @@ import { Memorize } from './06-memos/Memorize';
 import { MemorizeHook } from './06-memos/MemorizeHook';
 import { CallbackHook } from './06-memos/CallbackHook';
 import { TodoApp } from './07-useReducer/TodoApp';
+import { MainApp } from './08-useContext/MainApp';
 
 
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-   <TodoApp />
-  </React.StrictMode>,
+    <BrowserRouter>
+      <MainApp />
+    </BrowserRouter>
+  </React.StrictMode>
 );
